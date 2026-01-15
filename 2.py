d8ac7a9b5f873cac98c1e67241377e0a9023bc0d
Fekete Jenő <feketej@.kkszki.hu>
2026-01-15 18:41:40 +0100

    if, elif else ág. 2py futtatható

--- 2.py
+++ 2.py
@@ -4,3 +4,10 @@ Feladat: Kérj be három számot, majd írd ki, hogy mindhárom páros, mindhár
 szam1=int(input("Kérem az első számot: "))
 szam2=int(input("Kérem a második számot: "))
 szam3=int(input("kérem a harmadik számot: "))
+
+if szam1 % 2 == 0 and szam2 % 2 == 0 and szam3 % 2 == 0:
+    print("Mindhárom szám páros szám.")
+elif szam1 %2 == 1 and szam2 % 2 == 1 and szam3 % 2 == 1:
+    print("Mindhárom szám páratlan szám.")
+else:
+    print("A számok kevertek.")
