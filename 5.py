c96525e73d943ffb3e9213b4b3e72bb164f96d72
Fekete Jenő <feketej@.kkszki.hu>
2026-01-15 19:00:07 +0100

    Ez még dél körül elkészült.

---
+++ 5.py
@@ -0,0 +1,5 @@
+"""5. feladat: Hozz létre egy Diak osztályt a következőkkel: - Attribútumok:
+nev (string), eletkor (int), jegyek (lista int) - Metódusok:
+- atlag() – visszaadja a jegyek átlagát
+- bemutatkozas() – kiírja: &quot;Szia, [név] vagyok, [életkor] éves.&quot;
+- szinten() – visszaadja, hogy a diák Jó, Közepes vagy Gyenge a jegyek átlaga alapján."""
