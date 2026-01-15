04b97b73546e5ef160cdc2dc2bae50c045ec53f8
Fekete Jenő <feketej@.kkszki.hu>
2026-01-15 18:42:57 +0100

    A harmadik feladat

---
+++ 3.py
@@ -0,0 +1,3 @@
+"""3. feladat – Iteráció (for loop) + lista kezelése
+Feladat: Kérj be egy egész számot n, majd generálj egy listát az 1-től n-ig terjedő számokból. Írd ki, mely számok oszthatók 3-mal, és add össze ezeket."""
+
