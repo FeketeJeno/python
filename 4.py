7a2d5405c5cad14a6f0d675801dd3e38c906af4c
Fekete Jenő <feketej@.kkszki.hu>
2026-01-15 18:54:41 +0100

    bekéri a mondatot, Magánhangzók mh-ban, darabszám 0.

--- 4.py
+++ 4.py
@@ -1,2 +1,7 @@
 """4. feladat - Iteráció + feltételes logika + string kezelés
 Feladat: Kérj be egy mondatot, majd számold meg a magánhangzók számát és listázd ki, hogy melyik magánhangzó hányszor fordul elő."""
+mondat = input("Írj be egy mondatot: ")
+
+mgh = {}
+mh = "aáeéiíoóöőuúüű"
+mghDarab = 0
