aed161537b0650a5df220ed12314ca3e9ea2349f
Fekete Jenő <feketej@.kkszki.hu>
2026-01-15 19:01:12 +0100

    Diak class+attribútumok létrehozása

--- 5.py
+++ 5.py
@@ -3,3 +3,8 @@ nev (string), eletkor (int), jegyek (lista int) - Metódusok:
 - atlag() – visszaadja a jegyek átlagát
 - bemutatkozas() – kiírja: &quot;Szia, [név] vagyok, [életkor] éves.&quot;
 - szinten() – visszaadja, hogy a diák Jó, Közepes vagy Gyenge a jegyek átlaga alapján."""
+class Diak:
+    def __init__(self,nev,eletkor,jegyek):
+        self.nev = nev
+        self.eletkor = eletkor
+        self.jegyek = jegyek
