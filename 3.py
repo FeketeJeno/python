fe3546b2affc6a3e0d22ea2200df9abe3431b5c8
Fekete Jenő <feketej@.kkszki.hu>
2026-01-15 18:45:33 +0100

    a program leírja a feladatot, bekér egy számot

--- 3.py
+++ 3.py
@@ -1,3 +1,11 @@
 """3. feladat – Iteráció (for loop) + lista kezelése
 Feladat: Kérj be egy egész számot n, majd generálj egy listát az 1-től n-ig terjedő számokból. Írd ki, mely számok oszthatók 3-mal, és add össze ezeket."""
 
+lista=[]
+osszeg=0
+print("1-től a megadott számig ellenőrzöm a 3-mal osztható számokat.")
+n=int(input("Kérek egy egész számot,: "))
+for i in range(1,n+1):
+    lista.append(i)
+
+
