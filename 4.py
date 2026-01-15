2e659cfaf7df73110b673e9ad3f9ede558eb9be7
Fekete Jenő <feketej@.kkszki.hu>
2026-01-15 18:53:23 +0100

    A negyedik feladat

---
+++ 4.py
@@ -0,0 +1,2 @@
+"""4. feladat - Iteráció + feltételes logika + string kezelés
+Feladat: Kérj be egy mondatot, majd számold meg a magánhangzók számát és listázd ki, hogy melyik magánhangzó hányszor fordul elő."""
