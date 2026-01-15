3d54203b8ba9fb4398cd0c54a367d25347b3c821
Fekete Jenő <feketej@.kkszki.hu>
2026-01-15 18:39:39 +0100

    3 szám bekérése

--- 2.py
+++ 2.py
@@ -1,2 +1,6 @@
 """2. feladat – Szelekció + logikai operátorok
 Feladat: Kérj be három számot, majd írd ki, hogy mindhárom páros, mindhárom páratlan, vagy kevert."""
+
+szam1=int(input("Kérem az első számot: "))
+szam2=int(input("Kérem a második számot: "))
+szam3=int(input("kérem a harmadik számot: "))
